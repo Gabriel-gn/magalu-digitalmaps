88ffec25a4cc909e6ebb034b5a25d272cf035df5
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2020-03-03 23:56:26 -0300

    :pencil: Documentação e pequeno ajuste de layout

--- Frontend/src/app/services/http/http.service.ts
+++ Frontend/src/app/services/http/http.service.ts
@@ -9,7 +9,23 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   *
+   * @param posX: posição X do usuário
+   * @param posY: posição Y do usuário
+   * @param mts: distância máxima de locais relativos à posição do usuário
+   * @param hr: horário para verificação se o local está aberto ou fechado
+   */
   getLocationsByUserInput(posX: number, posY: number, mts: number, hr: string) {
     return this.http.get(`http://${environment.backend}/locais/user?pos_x=${posX}&pos_y=${posY}&mts=${mts}&hr=${hr}`);
+  }
+
+  /**
+   * Retorna todas as localizações pois a distância em mts é muito alta
+   * TODO fazer a distância em mts ser igual ao maior valor presente no banco de dados
+   * @param hr Deve-se fornecer a hora em formato hh:mm:ss string.
+   */
+  getAllLocations(hr: string) {
+    return this.http.get(`http://${environment.backend}/locais/user?pos_x=0&pos_y=0&mts=2000000000&hr=${hr}`);
   }
 }
